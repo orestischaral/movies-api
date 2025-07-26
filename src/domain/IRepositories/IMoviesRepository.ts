3420d17f55bcd7220b6677a2b9f5b84e9410e2b8
Orestis Charalampakos <orestischaral@gmail.com>
2025-07-26 15:08:29 +0300

    Implement IRepository

---
+++ src/domain/IRepositories/IMoviesRepository.ts
@@ -0,0 +1,13 @@
+import { Movie } from "../model/Movie";
+
+export default interface IMovieRepository {
+  getPopularMovies(limit?: number, skip?: number): Promise<Movie[]>;
+  searchMoviesInDb(
+    query: string,
+    sortBy: "rating" | "release_date",
+    genreFilter?: string,
+    yearFilter?: string,
+    skip?: number,
+    take?: number
+  ): Promise<Movie[]>;
+}
